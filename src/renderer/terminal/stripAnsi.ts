d4e37f155ccac0884b2cf3d7f0856a13890b1464
singhvibhanshu <singhvibhanshu@hotmail.com>
2026-03-08 15:55:57 +0530

    added support for ST-terminated ANSI seq.

--- src/renderer/terminal/stripAnsi.ts
+++ src/renderer/terminal/stripAnsi.ts
@@ -1,3 +1,5 @@
 export function stripAnsi(data: string): string {
-  return data.replace(/\x1b\[[0-9;]*[A-Za-z]/g, '').replace(/\x1b\][^\x07]*\x07/g, '');
+  return data
+    .replace(/\x1b\[[\x20-\x3F]*[\x40-\x7E]/g, '')
+    .replace(/\x1b\][\s\S]*?(\x07|\x1b\\)/g, '');
 }
